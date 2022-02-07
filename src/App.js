15666e1a3e92f2a7c79f20a1baacfe1f3a6bd031
Ricardo Atakiama <atakiama@usp.br>
2022-02-07 16:19:33 -0300

    Adicionando estilos com sass. Inclusão de placeholder no textarea

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Registration from './components/Registration';
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 
-import './Registration.css'
+import './Registration.scss'
 
 const Registration = () => {
   const [state, setState] = useState({
     name: 'Ex: Ricardo Atakiama',
-    description: 'Faça um resumo de suas qualificações, habilidades e realizações profissionais.',
+    description: '',
     maritalStatus: 'Single',
     birthDate: ''
   });
+
+  // https://restcountries.com/v3.1/all
 
   const handleChange = (e) => {
     console.log(e)
@@ -46,52 +48,60 @@ const Registration = () => {
   }
 
   return ( 
-    <form className='form-container' onSubmit={(e) => handleSubmit(e)} >
-      
-      <div className='form-header'><strong><p>Currículo</p></strong></div>
+    <div className='form-wrapper'>
+      <form className='form-container' onSubmit={(e) => handleSubmit(e)} >
+        
+        <div className='form-header'><strong><p>Currículo</p></strong></div>
 
-      <div className='data-set'><p>Dados pessoais</p></div>
+        <div className='data-set'><p>Dados pessoais</p></div>
 
-      <div className='form-side-by-side'>
-        <div className='form--left-side'>
+        <div className='form-side-by-side'>
+          <div className='form--left-side'>
+            <label className='field--title' htmlFor="fullName">Nome: </label>
+            <input className='field--input' name="fullName" type="text" value={state.name} onChange={handleChange}/>
+          </div>
+
+          <div className='form--right-side'>
+            <label className='field--title' htmlFor="maritalStatus">Estado civil: </label>
+            <select className='field--input' name="maritalStatus" id="maritalStatus" value={state.maritalStatus} onChange={handleChange}>
+              <option value="Single">Solteiro(a)</option>
+              <option value="Married">Casado(a)</option>
+              <option value="Separate">Separado(a)</option>
+              <option value="Divorced">Divorciado(a)</option>
+              <option value="Widowed">Viúvo(a)</option>
+            </select>
+          </div>
+        </div>
+        <div className='form-side-by-side'>
+
+          <div className='form--left-side'>
+            <label className='field--title' htmlFor="birthDate">Data de nascimento: </label>
+            <input className='field--input date-input' name="birthDate" type="date" value={state.birthDate} onChange={handleChange}/>
+          </div>
+
+          <div className='form--right-side'>
+            <label className='field--title' htmlFor="description">Descrição profissional: </label>
+            <textarea className='field--input' name="description" id="description" cols="30" rows="10" value={state.description} onChange={handleChange}
+              placeholder='Faça um resumo de suas qualificações, habilidades e realizações profissionais.'
+            ></textarea>
+          </div>
+        </div>
+            
+        {/* <div className='field'>
           <label className='field--title' htmlFor="fullName">Nome: </label>
           <input className='field--input' name="fullName" type="text" value={state.name} onChange={handleChange}/>
+        </div> */}
+
+        <div className='form-footer'>
+          <button type="submit" className='submit-button'>
+            Enviar
+          </button>
         </div>
 
-        <div className='form--right-side'>
-          <label className='field--title' htmlFor="maritalStatus">Estado civil: </label>
-          <select className='field--input' name="maritalStatus" id="maritalStatus" value={state.maritalStatus} onChange={handleChange}>
-            <option value="Single">Solteiro(a)</option>
-            <option value="Married">Casado(a)</option>
-            <option value="Separate">Separado(a)</option>
-            <option value="Divorced">Divorciado(a)</option>
-            <option value="Widowed">Viúvo(a)</option>
-          </select>
-        </div>
-      </div>
-      <div className='form-side-by-side'>
+      </form>
 
-        <div className='form--left-side'>
-          <label className='field--title' htmlFor="birthDate">Data de nascimento: </label>
-          <input className='field--input date-input' name="birthDate" type="date" value={state.birthDate} onChange={handleChange}/>
-        </div>
+    </div>
 
-        <div className='form--right-side'>
-          <label className='field--title' htmlFor="description">Descrição profissional: </label>
-          <textarea className='field--input' name="description" id="description" cols="30" rows="10" value={state.description} onChange={handleChange}></textarea>
-        </div>
-      </div>
-          
-      {/* <div className='field'>
-        <label className='field--title' htmlFor="fullName">Nome: </label>
-        <input className='field--input' name="fullName" type="text" value={state.name} onChange={handleChange}/>
-      </div> */}
-
-      <div className='form-footer'>
-        <input type="submit" value={'Enviar'}/>
-      </div>
-
-    </form>
   );
 }
  
