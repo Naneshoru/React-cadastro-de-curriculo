b632258107c7d2f5e9e88527809a44ea8d09ea41
Ricardo Atakiama <atakiama@usp.br>
2022-05-10 17:01:52 -0300

    Propriedade do grid xs em vez de md, corrige os tamanhos menores que o breakpoint md

--- src/components/ConfirmPage.jsx
+++ src/components/ConfirmPage.jsx
@@ -62,11 +62,11 @@ const ConfirmInfo = ({ state }) => {
   return ( 
     <>
       <Grid container style={{ padding: '24px' }}>
-        <Grid item md={3} />
-        <Grid item container md={8} spacing={2}>
+        <Grid item xs={3} />
+        <Grid item container xs={8} spacing={2}>
           {Object.keys(state).map((field) => {
             return (
-              <Grid item key={field} md={12}>
+              <Grid item key={field} xs={12}>
                 <Typography align='left'>
                   <span className='property'>{ propertiesToPortuguese(field) + ': ' }</span> 
                   <span className='value'>{ valuesToPortuguese(formatIfDate(state[field])).toString() }</span>
@@ -76,7 +76,7 @@ const ConfirmInfo = ({ state }) => {
           })}
         </Grid>
 
-        <Grid item md />
+        <Grid item xs />
 
       </Grid>      
     </>
